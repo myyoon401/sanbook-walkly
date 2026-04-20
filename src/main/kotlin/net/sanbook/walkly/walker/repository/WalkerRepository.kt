045684d9c8e4b02f4bd2862357ab9adca2e45a9e
myyoon401 <myyoon401@gmail.com>
2026-04-20 23:37:34 +0900

    daily mission 1

--- src/main/kotlin/net/sanbook/walkly/walker/repository/WalkerRepository.kt
+++
@@ -1,6 +0,0 @@
-package net.sanbook.walkly.walker.repository
-
-import net.sanbook.walkly.walker.entity.Walker
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface WalkerRepository : JpaRepository<Walker, Long>
