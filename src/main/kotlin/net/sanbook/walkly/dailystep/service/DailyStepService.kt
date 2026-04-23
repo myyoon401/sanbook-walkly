a174a52af42b728d35d04c0e5beef23a58876a2a
윤미영 <myyoon@huray.net>
2026-04-24 00:17:38 +0900

    [mission3][add] get dailystepSummary api

--- src/main/kotlin/net/sanbook/walkly/dailystep/service/DailyStepService.kt
+++ src/main/kotlin/net/sanbook/walkly/dailystep/service/DailyStepService.kt
@@ -2,7 +2,10 @@ package net.sanbook.walkly.dailystep.service
 
 import net.sanbook.walkly.account.service.AccountService
 import net.sanbook.walkly.dailystep.data.DailyStepResponse
+import net.sanbook.walkly.dailystep.data.DailyStepSummaryDetail
+import net.sanbook.walkly.dailystep.data.DailyStepSummaryResponse
 import net.sanbook.walkly.dailystep.data.UpsertDailyStepRequest
+import net.sanbook.walkly.dailystep.entity.DailyStep
 import net.sanbook.walkly.dailystep.mapper.toEntity
 import net.sanbook.walkly.dailystep.mapper.toResponse
 import net.sanbook.walkly.dailystep.repository.DailyStepRepository
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 @Service
@@ -30,10 +34,35 @@ class DailyStepService(
         return dailyStepRepository.save(dailyStep).toResponse()
     }
 
-    fun getDailyStepByDate(accountId: UUID, date: LocalDate?): DailyStepResponse {
-        val searchDate = date ?: LocalDate.now()
-        val dailyStep = dailyStepRepository.findByAccountIdAndDate(accountId, searchDate)
-            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "해당사용자의$searchDate 걸음수는 존재하지 않습니다.")
+    fun getDailyStepByDate(accountId: UUID, date: LocalDate): DailyStepResponse {
+        val dailyStep = dailyStepRepository.findByAccountIdAndDate(accountId, date)
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "해당사용자의$date 걸음수는 존재하지 않습니다.")
         return dailyStep.toResponse()
+    }
+
+    fun getDailyStepSummaryBetween(accountId: UUID, from: LocalDate, to: LocalDate): DailyStepSummaryResponse {
+        require(!from.isAfter(to)) { "from은 to보다 늦을 수 없습니다" }
+        require(ChronoUnit.DAYS.between(from, to) <= 30) { "최대 31일까지 조회 가능" }
+
+        accountService.validateAccountExists(accountId)
+
+        val dailySteps = dailyStepRepository.findAllByAccountIdAndDateBetween(accountId, from, to)
+        val totalSteps: Int = dailySteps.sumOf { it.stepCount }
+
+        return if (dailySteps.isEmpty()) {
+            DailyStepSummaryResponse(accountId, from, to, 0, 0, 0, null)
+        } else {
+            val maxDay = dailySteps.maxByOrNull { it.stepCount }
+                ?.let { DailyStepSummaryDetail(it.date, it.stepCount) }
+            DailyStepSummaryResponse(
+                accountId,
+                from,
+                to,
+                dailySteps.size,
+                totalSteps,
+                totalSteps / dailySteps.size,
+                maxDay
+            )
+        }
     }
 }
